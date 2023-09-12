0e80d70de8ba9325ea5aca6c746933d1dcd88b49
Brianna Tran <briannatran45@gmail.com>
2023-09-12 16:23:32 -0700

    finished getChains

--- markov.js
+++ markov.js
@@ -27,36 +27,39 @@ class MarkovMachine {
    * */
 
   getChains() {
-    // TODO: implement this!
-
-    const choices = {
-    };
     /**
      * for every word, go through all the text, and whenever, there is a word infront
      * that, that word will be a value inside a array of possible choices
-     *
-     * choices object should include keys with every word/punctuation and
-     * values that is list with all of the words that were one ahead of that key
-     *
-     * one key will have null value, for when text ends
-     *
-     * once we have the complete choice obj, we return it
-     *  */
+    *
+    * choices object should include keys with every word/punctuation and
+    * values that is list with all of the words that were one ahead of that key
+    *
+    * one key will have null value, for when text ends
+    *
+    * once we have the complete choice obj, we return it
+    *  */
 
-    for (let [i,phrase] of this.words.entries()){
-        if (!(phrase in choices)){
-          debugger;
-          choices[phrase] = [this.words[i+1]];
-        }else if (!this.words[i+1]){
+    // !this.words[i + 1]
+    const choices = {};
+
+    for (let [i, phrase] of this.words.entries()) {
+      if (i === this.words.length - 1) {
+        if (phrase in choices) {
           choices[phrase].push(null);
-        }else{
-
-          choices[phrase].push(this.words[i+1]);
         }
+        else {
+          choices[phrase] = [null];
+        }
+      }
+      else if (!(phrase in choices)) {
+        choices[phrase] = [this.words[i + 1]];
+      }
+      else {
+        choices[phrase].push(this.words[i + 1]);
+      }
     }
-    console.log(choices);
 
-
+    return choices;
   }
 
 
@@ -69,9 +72,11 @@ class MarkovMachine {
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
+
+    //this.chains
   }
 }
-let machine = new MarkovMachine("The cat in the hat.");
+let machine = new MarkovMachine("The cat in the hat. The cat is the hat.");
 
 module.exports = {
   MarkovMachine,
