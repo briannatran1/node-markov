3d52b1b995d33460287a0d9ef1d9645a7e4724f2
Jung-GunSong <jsong781@gmail.com>
2023-09-12 19:05:28 -0400

    debugging getChains()

--- markov.js
+++ markov.js
@@ -15,7 +15,7 @@ class MarkovMachine {
   /** Get markov chain: returns object of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,11 +23,40 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
     // TODO: implement this!
+
+    const choices = {
+    };
+    /**
+     * for every word, go through all the text, and whenever, there is a word infront
+     * that, that word will be a value inside a array of possible choices
+     *
+     * choices object should include keys with every word/punctuation and
+     * values that is list with all of the words that were one ahead of that key
+     *
+     * one key will have null value, for when text ends
+     *
+     * once we have the complete choice obj, we return it
+     *  */
+
+    for (let [i,phrase] of this.words.entries()){
+        if (!(phrase in choices)){
+          debugger;
+          choices[phrase] = [this.words[i+1]];
+        }else if (!this.words[i+1]){
+          choices[phrase].push(null);
+        }else{
+
+          choices[phrase].push(this.words[i+1]);
+        }
+    }
+    console.log(choices);
+
+
   }
 
 
@@ -42,3 +71,8 @@ class MarkovMachine {
     // - repeat until reaching the terminal null
   }
 }
+let machine = new MarkovMachine("The cat in the hat.");
+
+module.exports = {
+  MarkovMachine,
+};
